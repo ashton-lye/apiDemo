3eff1dc033efb60e0900ba3513602a54a6ac5abd
Ashton Lye <ashton@Ashtons-MacBook-Air.local>
2018-05-22 14:43:42 +1200

    extra feature added - getting local time of searched city

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 //GOOGLE PLACES API KEY:  AIzaSyBfr9AHIBiM-cj5XfxtP6Leh-k6X5Fm0d0 
+//TIMEZONEDB API KEY: IFJLIHI7IEVB
+//TIMEZONEDB DOCUMENTATION: https://timezonedb.com/references/get-time-zone
 
 //create map, geocoder and array here so we can use them everywhere
 var map;
@@ -6,16 +8,14 @@ var geocoder;
 var displayedLocation;
 var cityArray = [];
 
-function someJSFunction() {
-    alert('someJSFunction');
-}
-
 //constructor city object
-function cityObject(name, location, latitude, longitude, sunrise, sunset) {
+function cityObject(name, location, latitude, longitude, timezone, currentTime, sunrise, sunset) {
     this.name = name;
     this.location = location;
     this.latitude = latitude;
     this.longitude = longitude;
+    this.timezone = timezone;
+    this.currentTime = currentTime;
     this.sunrise = sunrise;
     this.sunset = sunset;
 };
@@ -81,40 +81,49 @@ function createCityObject(search) {
             var location = result[0].geometry.location;
             var latitude = result[0].geometry.location.lat();
             var longitude = result[0].geometry.location.lng();
+            var timezone;
+            var currentTime;
             var sunrise;
             var sunset;
 
             //create a new city object using the attributes specified above
-            var city = new cityObject(name, location, latitude, longitude, sunrise, sunset);    
-            
-            //sunrise-sunset API request to get sunrise/set times
-            ajaxRequest("POST", "sunset-sunrise.php", false, "lat="+city.latitude+"&long="+city.longitude, function(results) {
-                parsedResult = JSON.parse(results);   
-
-                //split timestamp strings to remove the AM and PM parts
-                var sunrise = parsedResult.results.sunrise;
-                var sunset = parsedResult.results.sunset;
-                var splitSunrise = sunrise.split(" ");
-                var splitSunset = sunset.split(" ");
-
-                //give the city object the sunrise and sunset values
-                city.sunrise = splitSunrise[0];
-                city.sunset = splitSunset[0];
-            });
-            //add the city object to the city array
-            cityArray.push(city);       
+            var city = new cityObject(name, location, latitude, longitude, timezone, currentTime, sunrise, sunset);  
 
             //create li elements for the searched city and add it to the list of searched cities
             var cityList = document.getElementById("cityList");
-            
+
             //turn city name into a string so it concatenates more easily in the li element
-            var nameString = '"'+city.name+'"';
+            var nameString = "'"+city.name+"'";
             //when the element is clicked, it calls the displayLocation function and passes in the name of the city to display
-            var element = "<li onclick='Javascript:displayLocation("+nameString+")'>"+city.name+"</li>";
+            var element = '<li onclick="Javascript:displayLocation('+nameString+')">'+city.name+'</li>';
             cityList.innerHTML += element;
 
-            //call function to display city data etc
-            displayLocation(city.name);
+            var dataList = document.getElementById("cityDataList");
+            dataList.innerHTML = "";
+            dataList.innerHTML = "<li><b>City Data:</b></li><li>Loading City Data...</li>";
+            
+            //timezoneDB request to get cities timezone and current time
+            ajaxRequest("POST", "timezone.php", true, "lat="+city.latitude+"&long="+city.longitude, function(results) {
+                parsedResult = JSON.parse(results);
+
+                //use returned data to set city properties
+                city.timezone = parsedResult.abbreviation;
+                city.currentTime = parsedResult.formatted;
+
+                //sunrise-sunset API request to get sunrise/set times
+                ajaxRequest("POST", "sunset-sunrise.php", true, "lat="+city.latitude+"&long="+city.longitude, function(results) {
+                    parsedResult = JSON.parse(results);   
+
+                    city.sunrise = new Date(parsedResult.results.sunrise);
+                    city.sunset = new Date(parsedResult.results.sunset);
+
+                    //add the city object to the city array
+                    cityArray.push(city);  
+
+                    //call function to display city data etc
+                    displayLocation(city.name);
+                });
+            });
         }
         else {
             alert('Geocoder search failed. Status code: ' + status);
@@ -140,9 +149,12 @@ function displayLocation(cityName) {
             "<li><b>City Data:</b></li>"
             +"<li>City Name: "+cityArray[i].name+"</li>"
             +"<li>Latitude/Longitude: "+cityArray[i].location+"</li>"
-            //+"<li>Current Time: WIP</li>"
-            +"<li>Sunrise Time: "+cityArray[i].sunrise+" AM</li>"
-            +"<li>Sunset Time: "+cityArray[i].sunset+" PM</li>";
+            +"<li>Local Time (at Time of Search): "+cityArray[i].currentTime+"</li>"
+            +"<li>Local Timezone: "+cityArray[i].timezone+"</li>"
+            +"<li>Sunrise (GMT): "+cityArray[i].sunrise.toUTCString()+"</li>"
+            +"<li>Sunset (GMT): "+cityArray[i].sunset.toUTCString()+"</li>"
+            +"<li>Sunrise (NZST): "+cityArray[i].sunrise+"</li>"
+            +"<li>Sunset (NZST): "+cityArray[i].sunset+"</li>";
         };
     };  
 };
@@ -162,14 +174,20 @@ function updateMap(coordinates){
 function displayPlaces(result) {
     var placeList = document.getElementById("placeList");
     placeList.innerHTML = "<li><b>Places of Interest:</b></li>";
-    var parsedPlaces = JSON.parse(result);
+    try {
+        var parsedPlaces = JSON.parse(result);
 
-    //loop through the array of places returned by the request and add an li element to the places list
-    for (var i = 0; i < parsedPlaces.results.length; i++) {
-        var locationString = JSON.stringify(parsedPlaces.results[i].geometry.location);
-        //the onclick function calls the update map function to show the selected place
-        var element = "<li onclick='Javascript:updateMap("+locationString+")'>"+parsedPlaces.results[i].name+" - "+parsedPlaces.results[i].vicinity+"</li>";
-        placeList.innerHTML += element;
+        //loop through the array of places returned by the request and add an li element to the places list
+        for (var i = 0; i < parsedPlaces.results.length; i++) {
+            var locationString = JSON.stringify(parsedPlaces.results[i].geometry.location);
+            //the onclick function calls the update map function to show the selected place
+            var element = "<li onclick='Javascript:updateMap("+locationString+")'>"+parsedPlaces.results[i].name+" - "+parsedPlaces.results[i].vicinity+"</li>";
+            placeList.innerHTML += element;
+        };
+    } catch {
+        alert("No results found! Please try a different Search term.");
     };
+  
+
 };
 
--- timezone.php
+++ timezone.php
@@ -2,7 +2,7 @@
     $lat = $_POST['lat'];
     $long = $_POST['long'];
 
-    $request = "https://api.sunrise-sunset.org/json?lat=$lat&lng=$long&formatted=0";
+    $request = "http://api.timezonedb.com/v2/get-time-zone?key=IFJLIHI7IEVB&format=json&by=position&lat=$lat&lng=$long";
 
     $connection = curl_init($request);
     curl_setopt($connection, CURLOPT_RETURNTRANSFER, true);
